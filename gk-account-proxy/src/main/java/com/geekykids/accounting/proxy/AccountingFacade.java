624bc95f96f874ef910ba748744449e72471cb05
Nick <nicholas.camilleri@geekykids.co.uk>
2017-05-22 21:58:24 +0100

    Initial Code

---
+++ gk-account-proxy/src/main/java/com/geekykids/accounting/proxy/AccountingFacade.java
@@ -0,0 +1,8 @@
+package com.geekykids.accounting.proxy;
+
+import com.geekykids.model.Client;
+
+public interface AccountingFacade {
+
+	void addClient(final Client client);
+}
---
+++ gk-account-proxy/src/test/java/com/geekykids/accounting/proxy/AccountingFacadeTest.java
@@ -0,0 +1,11 @@
+package com.geekykids.accounting.proxy;
+
+import org.junit.Test;
+
+public class AccountingFacadeTest {
+
+	@Test
+	public void test(){
+		
+	}
+}
